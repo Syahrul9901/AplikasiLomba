9c7971a626a5c69b0aff5e307ed6bfd248225be0
Syahrul9901 <syahrulwij99@gmail.com>
2025-04-11 08:37:08 +0700

    first commit

---
+++ resources/views/registrasi.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>registrasi</title>
+    <link rel="stylesheet" href="{{ asset ('bootstrap/css/bootstrap.css') }}">
+</head>
+<body>
+    <div>
+        <h2>Registrasi WebGame</h2>
+        <p>Silahkan isi form registrasi dibawah ini</p>
+    </div>
+    <div>
+        <div>
+            <div>
+                <div>
+                    <form action="">
+                        <label>Nama Lengkap</label>
+                        <input type="text" name="name" class="form-control mb-2">
+                        <label>Email Address</label>
+                        <input type="text" name="email" class="form-control mb-2">
+                        <label></label>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
